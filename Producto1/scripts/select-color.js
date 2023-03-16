db87a13e4df4b838610b807dcf0782183a6cdb83
LauRoConUOC <lrodriguezconta@uoc.edu>
2023-03-16 18:49:59 +0100

    panel de creacion de semana

---
+++ Producto1/scripts/select-color.js
@@ -0,0 +1,16 @@
+// Obtenemos todos los elementos circulo
+const circles = document.querySelectorAll('.circle');
+
+// Obtenemos el elemento textarea
+const descripcion = document.getElementById('descripcion');
+
+// Agregamos un event listener para cada círculo
+circles.forEach(circle => {
+    circle.addEventListener('click', () => {
+        // Obtenemos el color del círculo seleccionado
+        const color = getComputedStyle(circle).getPropertyValue('background-color');
+
+        // Cambiamos el fondo del área de texto al color elegido
+        descripcion.style.backgroundColor = color;
+    });
+});
