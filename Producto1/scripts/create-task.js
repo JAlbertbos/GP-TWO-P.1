eb4cb6ac6ade3cdf96847548a33e456d34a9ae7f
LauRoConUOC <lrodriguezconta@uoc.edu>
2023-03-22 20:25:52 +0100

    mas fixes

--- Producto1/scripts/create-task.js
+++ Producto1/scripts/create-task.js
@@ -12,15 +12,15 @@ document.addEventListener("DOMContentLoaded", () => {
     cardContainer.classList.add("col-md-4", "mb-4");
 
     const card = `
-      <div class="card shadow-sm card-square" data-id="${id}" style="border-color: ${selectedColor}">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-        </div>
-        <<div class="card-icons d-flex justify-content-between position-absolute bottom-0 start-0 end-0">
-          <a href="#" class="card-link view-icon"><i class="bi bi-eye"></i></a>
-          <a href="#" class="card-link delete-icon"><i class="bi bi-trash"></i></a>
-        </div>
-      </div>
+    <div class="card shadow-sm card-square" data-id="${id}" style="border-color: ${selectedColor || 'black'}">
+    <div class="card-body">
+      <h5 class="card-title">${title}</h5>
+    </div>
+    <div class="card-icons d-flex justify-content-between position-absolute bottom-0 start-0 end-0">
+      <a href="#" class="card-link view-icon"><i class="bi bi-eye"></i></a>
+      <a href="#" class="card-link delete-icon"><i class="bi bi-trash"></i></a>
+    </div>
+  </div>
     `;
 
     cardContainer.innerHTML = card;
