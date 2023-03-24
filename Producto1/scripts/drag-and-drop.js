a4a59ddadee931598e876f801222d6848b9f5e43
LauRoConUOC <lrodriguezconta@uoc.edu>
2023-03-24 13:54:43 +0100

    fixes

--- Producto1/scripts/drag-and-drop.js
+++ Producto1/scripts/drag-and-drop.js
@@ -1,17 +1,17 @@
 function allowDrop(event) {
-    event.preventDefault();
-  }
+  event.preventDefault();
+}
 
-  function drag(event) {
-    event.dataTransfer.setData("text", event.target.id);
-  }
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
 
-  function drop(event) {
-    event.preventDefault();
-    const data = event.dataTransfer.getData("text");
-    const target = event.target;
-  
-    if (target.classList.contains("dropzone")) {
-      target.appendChild(document.getElementById(data));
-    }
+function drop(event) {
+  event.preventDefault();
+  const data = event.dataTransfer.getData("text");
+  const target = event.target;
+
+  if (target.classList.contains("dropzone")) {
+    target.appendChild(document.getElementById(data));
   }
+}
