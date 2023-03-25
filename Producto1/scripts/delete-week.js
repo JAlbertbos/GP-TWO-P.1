b46524300c018c9b17de57cf2d6813387d21934a
LauRoConUOC <lrodriguezconta@uoc.edu>
2023-03-25 20:09:58 +0100

    Ultimando detalles

--- Producto1/scripts/delete-week.js
+++ Producto1/scripts/delete-week.js
@@ -1,49 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Obtén todos los elementos con la clase 'bi-trash'
-    const trashIcons = document.querySelectorAll(".bi-trash");
-  
-    // Variable global para almacenar el ID de la carpeta que se va a eliminar
-    let folderIdToDelete = null;
-  
-    // Agrega un evento de clic a cada icono de basura
-    trashIcons.forEach((icon) => {
-      icon.addEventListener("click", function (event) {
-        // Encuentra el elemento carpeta más cercano
-        const folder = event.target.closest(".folder");
-  
-        // Establece el valor del atributo data-folder-id en la variable global
-        folderIdToDelete = folder.getAttribute("data-folder-id");
-      });
-    });
-  
-    // Agrega un nuevo selector que busque los iconos de basura en las nuevas carpetas creadas en el modal
-    const modalFolderTrashIcons = document.querySelectorAll("#nuevaCarpetaModal .bi-trash");
-  
-    // Agrega un evento de clic a cada icono de basura en las nuevas carpetas creadas en el modal
-    modalFolderTrashIcons.forEach((icon) => {
-      icon.addEventListener("click", function (event) {
-        // Encuentra el elemento carpeta más cercano
-        const folder = event.target.closest(".modal-folder");
-  
-        // Elimina la carpeta
-        folder.parentNode.removeChild(folder);
-      });
-    });
-  
-    // Agrega un evento de clic al botón 'Eliminar' del modal de eliminación de carpeta
-    document.getElementById("eliminarCarpetaBtn").addEventListener("click", function () {
-      // Encuentra la carpeta que se va a eliminar
-      const folderToDelete = document.querySelector(`[data-folder-id="${folderIdToDelete}"]`);
-  
-      // Elimina la carpeta
-      folderToDelete.parentNode.removeChild(folderToDelete);
-  
-      // Resetea el valor de la variable global
-      folderIdToDelete = null;
-  
-      // Cierra el modal de eliminación de carpeta
-      const eliminarCarpetaModal = document.getElementById("eliminarCarpetaModal");
-      const modal = bootstrap.Modal.getInstance(eliminarCarpetaModal);
-      modal.hide();
+document.addEventListener("DOMContentLoaded", () => {
+  let selectedCard = null;
+
+  // Escuchar eventos de click en los iconos de eliminar
+  const deleteIcons = document.querySelectorAll(".bi-trash");
+  deleteIcons.forEach((icon) => {
+    icon.addEventListener("click", (event) => {
+      const card = event.target.closest(".card");
+      selectedCard = card;
     });
   });
+
+  // Escuchar evento de click en el botón de eliminar tarjeta
+  const deleteCardBtn = document.querySelector("#eliminarTarjetaBtn");
+  deleteCardBtn.addEventListener("click", () => {
+    if (selectedCard) {
+      selectedCard.closest('.col-md-4.mb-4').remove();
+      selectedCard = null;
+      const eliminarTarjetaModalEl = document.getElementById("eliminarTarjetaModal");
+      const eliminarTarjetaModal = bootstrap.Modal.getInstance(eliminarTarjetaModalEl);
+      eliminarTarjetaModal.hide();
+    }
+  });
+});
+
+
+
+
