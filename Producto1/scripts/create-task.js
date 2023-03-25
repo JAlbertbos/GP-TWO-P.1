8264226dd3a312c340fc8ca8369e702db27c2fa4
KaidouoftheBeasts <dgomeztom@uoc.edu>
2023-03-25 14:18:22 +0100

    Verificación del formulario y muestra de detalles de la semana
    Verificación del formulario en ventana modal y se muestran los detalles de la semana.

--- Producto1/scripts/create-task.js
+++ Producto1/scripts/create-task.js
@@ -7,14 +7,18 @@ document.addEventListener("DOMContentLoaded", () => {
     return Math.floor(Math.random() * 1000000);
   }
 
-  function createCard(title, id) {
+  function createCard(name, id, day, month, year, description) {
     const cardContainer = document.createElement("div");
     cardContainer.classList.add("col-md-4", "mb-4");
 
     const card = `
-    <div class="card shadow-sm card-square" data-id="${id}" style="border-color: ${selectedColor || 'black'}">
+    <div class="card shadow-sm card-square" data-id="${id}" style="border-color: ${selectedColor}">
     <div class="card-body">
-      <h5 class="card-title">${title}</h5>
+      <h5 class="card-title"><b>${name}</b></h5>
+      <p class ="card-text">Día: ${day}</p>
+      <p class ="card-text">Mes: ${month}</p>
+      <p class ="card-text">Año: ${year}</p>
+      <p class ="card-text">Descripcion: ${description}</p>
     </div>
     <div class="card-icons d-flex justify-content-between position-absolute bottom-0 start-0 end-0">
       <a href="#" class="card-link view-icon"><i class="bi bi-eye"></i></a>
@@ -41,20 +45,61 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   confirmBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+    var formulario = document.getElementById("cardForm");
+    var inputsRequeridos = formulario.querySelectorAll("[required]");
+  
+    var valido = true;
+  
+    for (var i = 0; i < inputsRequeridos.length; i++) {
+      if (!inputsRequeridos[i].value) {
+        valido = false;
+        break;
+      }
+    }
+  
+    if (valido) {
+      e.preventDefault();
+      let name = document.getElementById("name").value;
+      let day = document.getElementById("day").value;
+      let month = document.getElementById("month").value;
+      let year = document.getElementById("year").value;
+      let description = document.getElementById("description").value;
 
-    const title = cardForm.querySelector("input[type='text']").value;
-    const id = generateRandomId();
+      // Expresión regular para validar el valor del campo de día
+      const dayRegex = /^(0?[1-9]|[12][0-9]|3[01])$/;
+      if (!dayRegex.test(day)) {
+        alert("Por favor ingrese un día válido (entre 1 y 31).");
+       return;
+      }
 
-    createCard(title, id);
+      // Expresión regular para validar el valor del campo de mes
+      const monthRegex = /^(0?[1-9]|1[0-2])$/;
+      if (!monthRegex.test(month)) {
+        alert("Por favor ingrese un mes válido (entre 1 y 12).");
+        return;
+      }
 
-    // Cerrar el modal
-    const nuevaSemanaModal = document.getElementById("nuevaSemanaModal");
-    const modal = bootstrap.Modal.getInstance(nuevaSemanaModal);
-    modal.hide();
+      // Expresión regular para validar el valor del campo de año
+      const yearRegex = /^\d{4}$/;
+      if (!yearRegex.test(year)) {
+        alert("Por favor ingrese un año válido (formato: AAAA).");
+        return;
+      }
 
-    // Limpiar el formulario
-    cardForm.reset();
+      const id = generateRandomId();
+
+      createCard(name, id, day, month, year, description);
+
+      // Cerrar el modal
+      const nuevaSemanaModal = document.getElementById("nuevaSemanaModal");
+      const modal = bootstrap.Modal.getInstance(nuevaSemanaModal);
+      modal.hide();
+
+      // Limpiar el formulario
+      cardForm.reset();
+    } else {
+      alert("Por favor complete todos los campos requeridos.");
+    }
   });
 
   document.querySelectorAll(".delete-icon").forEach((deleteIcon) => {
