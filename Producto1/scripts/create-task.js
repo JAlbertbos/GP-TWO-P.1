6cb2d32e06bde1b1a6dd1438d462377a25233e8a
Jorge A <101210883+JAlbertbos@users.noreply.github.com>
2023-03-30 08:10:23 +0200

    cambio

--- Producto1/scripts/create-task.js
+++ Producto1/scripts/create-task.js
@@ -7,6 +7,8 @@ function drop(event) {
   var element = document.getElementById(data);
   event.target.appendChild(element);
 }
+
+let tarjetaAEditar;
 let selectedDay;
 // Controlador de eventos para los botones
 document.querySelectorAll('[data-day]').forEach(button => {
@@ -15,8 +17,6 @@ document.querySelectorAll('[data-day]').forEach(button => {
   });
 });
 
-  
- 
 
 // Obtener elementos del formulario
 const form = document.querySelector('#formtask form');
@@ -29,9 +29,29 @@ const ubicacion = document.querySelector('#ubicacion');
 const tareaTerminada = document.querySelector('#tareaTerminada');
 const iconoPapelera = document.createElement('i');
 iconoPapelera.classList.add('bi', 'bi-trash-fill', 'ms-2', 'eliminar-tarea', 'text-danger');
+
+
 // Controlador de eventos para el formulario
 form.addEventListener('submit', function (event) {
   event.preventDefault();
+
+  if (tarjetaAEditar) {
+    // Actualizar la tarjeta existente
+    tarjetaAEditar.querySelector('.card-title').innerText = nombreTarea.value;
+    tarjetaAEditar.querySelector('.card-text').innerText = descripcion.value;
+    tarjetaAEditar.querySelector('.list-group-item:nth-child(1)').innerText = `Hora de inicio: ${horaInicio.value}`;
+    tarjetaAEditar.querySelector('.list-group-item:nth-child(2)').innerText = `Hora de final: ${horaFinal.value}`;
+    tarjetaAEditar.querySelector('.list-group-item:nth-child(3)').innerText = `Participantes: ${participantes.value}`;
+    tarjetaAEditar.querySelector('.list-group-item:nth-child(4)').innerText = `Ubicación: ${ubicacion.value}`;
+    tarjetaAEditar.querySelector('.form-check-input').checked = tareaTerminada.checked;
+
+    // Reiniciar la variable tarjetaAEditar
+    tarjetaAEditar = null;
+
+    const modal = bootstrap.Modal.getInstance(document.querySelector('#formtask'));
+    modal.hide();
+    form.reset();
+  } else {
 
   // Crear la tarjeta con los datos del formulario
   const tarjeta = document.createElement('div');
@@ -96,18 +116,11 @@ form.addEventListener('submit', function (event) {
     }
   });
 
-  let tarjetaCreada;
-  // Lapiz edicion
-  const botonEditar = tarjeta.querySelector('.editar-tarea');
-  botonEditar.addEventListener('click', function () {
-    // Obtener los elementos del formulario
-    const nombreTareaEdit = document.querySelector('#nombreTarea');
-    const descripcionEdit = document.querySelector('#descripcion');
-    const horaInicioEdit = document.querySelector('#horaInicio');
-    const horaFinalEdit = document.querySelector('#horaFinal');
-    const participantesEdit = document.querySelector('#participantes');
-    const ubicacionEdit = document.querySelector('#ubicacion');
-    const tareaTerminadaEdit = document.querySelector('#tareaTerminada');
+   // Lapiz edicion
+    const botonEditar = tarjeta.querySelector('.editar-tarea');
+    botonEditar.addEventListener('click', function () {
+    // Guardar la referencia a la tarjeta que se va a editar
+   tarjetaAEditar = tarjeta;
 
     // Obtener la información de la tarjeta creada
     const titulo = tarjeta.querySelector('.card-title').innerText;
@@ -132,84 +145,6 @@ form.addEventListener('submit', function (event) {
     modal.show();
   });
 
-
-  const confirmBtn = document.getElementById("saveButton");
-  
-  // Generar el mensaje de error por falta de campos requeridos
-  function mostrarModal(mensaje) {
-    const modal = new bootstrap.Modal(document.getElementById("genericaModal"));
-    const mensajeModal = document.getElementById("genericaModalMessage");
-    mensajeModal.innerText = mensaje;
-    modal.show();
-  }
-
-  confirmBtn.addEventListener("click", (e) => {
-    var formulario = document.getElementById("formtask");
-    var inputsRequeridos = formulario.querySelectorAll("[required]");
-
-    var valido = true;
-
-    for (var i = 0; i < inputsRequeridos.length; i++) {
-      if (!inputsRequeridos[i].value) {
-        valido = false;
-        break;
-      }
-    }
-
-    if (valido) {
-      e.preventDefault();
-      let name = document.getElementById("nombreTarea").value;
-      let initTime = document.getElementById("horaInicio").value;
-      let description = document.getElementById("descripcion").value;
-
-
-
-      // Expresión regular para validar el nombre de tarea
-      // validar  Nombre
-      const nameRegex = /^[a-zA-ZáéíóúñÁÉÍÓÚÑ\s]+$/;
-      if (!nameRegex.test(name)) {
-        mostrarModal("Por favor ingrese solo letras.");
-        return;
-      }
-
-      // Expresión regular para validar la hora de inicio
-      const initTimeRegex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
-      if (!initTimeRegex.test(initTime)) {
-        mostrarModal("Por favor ingrese una hora de inicio válida en formato de 24 horas (por ejemplo, 09:30).");
-        return;
-      }
-
-
-      // Expresión regular para validar la descripción
-      // validar  Nombre
-      const descriptionRegex = /^[a-zA-ZáéíóúñÁÉÍÓÚÑ\s]+$/;
-      if (!descriptionRegex.test(description)) {
-        mostrarModal("Por favor ingrese solo letras.");
-        return;
-      }
-
-
-
-      const id = generateRandomId();
-
-      createCard(name, id, day, month, year, description);
-
-      // Cerrar el modal
-      const nuevaTareaModal = document.getElementById("nuevaTareaModal");
-      const modal = bootstrap.Modal.getInstance(nuevaTareaModal);
-      modal.hide();
-
-      // Limpiar el formulario
-      cardForm.reset();
-    } else {
-      mostrarModal("Faltan campos por completar");
-    }
-  });
-  document.querySelectorAll(".delete-icon").forEach((deleteIcon) => {
-    deleteIcon.addEventListener("click", (e) => {
-      e.preventDefault();
-      const cardContainer = e.target.closest(".mb-3");
-      deleteCard(cardContainer);
-    });
-  });
+}
+form.reset(); // Reiniciar formulario para edición sin bugs!
 });
